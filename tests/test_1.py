4e5edddd26caa1fb0b438b3caf899ab5449eccba
Carlos Gloria <a00574714@tec.mx>
2026-09-13 01:13:10 -0600

    wdw

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,13 +1,25 @@
 import xgboost as xgb
 import pandas as pd
 import glob
+import os
+import sys
 from pathlib import Path
+from pymongo import MongoClient
+from datetime import datetime, timezone
+
+# Agrega la carpeta raíz (HackMty_2026) al PATH de Python
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Importa tus módulos de procesamiento (ajusta las rutas según tu estructura)
 from src.fase1.main import procesar_audio_base, recortar_voz_activa
 from src.fase2.fase2_acustica import aplicar_filtro_pasabanda, extraer_metricas_acusticas
 from src.fase3.fase3_conversacional import extraer_metricas_tiempo
 from src.tools.vad import generar_turnos_vad, fusionar_turnos # Asegúrate de tener la función de fusión aquí
+
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://adminAltur:adminaltur67@clusteraltur.6yjrmxp.mongodb.net/?appName=ClusterAltur")
+cliente_mongo = MongoClient(MONGO_URI) 
+db = cliente_mongo["proyectoAltur"]
+coleccion = db["historial_predicciones"]
 
 def probar_audios_locales(directorio_muestras, ruta_modelo="modelo_xgboost_altur_v4.json"):
     # 1. Cargar el modelo XGBoost
@@ -26,6 +38,7 @@ def probar_audios_locales(directorio_muestras, ruta_modelo="modelo_xgboost_altur
 
     for ruta_audio in archivos_wav:
         nombre_archivo = Path(ruta_audio).name
+        nombre_sin_extension = nombre_archivo.replace('.wav', '')
         try:
             # --- PIPELINE DE PROCESAMIENTO ---
             
@@ -80,6 +93,20 @@ def probar_audios_locales(directorio_muestras, ruta_modelo="modelo_xgboost_altur
                 veredicto = "👤 Human"
             
             confianza = prob_ia * 100 if prediccion == 1 else (1 - prob_ia) * 100
+
+            documento = { #objeto de mongodb
+                "id": nombre_sin_extension,
+                "resultado": prediccion,
+                "confianza": prob_ia, 
+                "fecha_analisis": datetime.now(timezone.utc)
+            }
+            
+            try: #publicacion a la db
+                coleccion.insert_one(documento)
+                estado_db = "💾 Guardado en Atlas"
+                print(f"✅ Guardado en DB: {nombre_archivo}")
+            except Exception as e:
+                estado_db = f"⚠️ Error DB: {e}"
             
             print(f" Vector generado: {fila}")
             print(f"🎙️ {nombre_archivo}")
